8fc9904253c4a06dcbed0eed6d346551852f1f5e
meelbaperez <merima001@hotmail.com>
2021-11-04 18:44:07 +0000

    Segunda versión de kata4

--- src/kata4/main/KATA4.java
+++ src/kata4/main/KATA4.java
@@ -8,13 +8,31 @@ import kata4.view.MailHistogramBuilder;
 import kata4.view.MailListReader;
 
 public class KATA4 {
+    private static List<Mail> mailList;
+    private static Histogram<String> histogram;
+
+    public static void main(String[] args){
+        execute(); 
+    }
     
-    public static void main(String[] args) {
+    public static void input(){
         String fileName = new String("email.txt");
-        List<Mail> mailList = MailListReader.read(fileName);
-        Histogram<String> histogram = MailHistogramBuilder.built(mailList);
+        mailList = MailListReader.read(fileName);
+    }
+    
+    public static void process(){
+       histogram = MailHistogramBuilder.built(mailList);
+    }
+    
+    public static void output(){
         HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
         histoDisplay.execute();
+    }
+    
+    public static void execute(){
+        input();
+        process();
+        output();
     }
 
 }
